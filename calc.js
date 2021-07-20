f65664d52690deb5d08a062f0d1c4cf4a8f0b48e
Brandon <69767056+OtakuxCode@users.noreply.github.com>
2021-07-19 21:52:55 -0400

    Add files via upload

--- calc.js
+++ calc.js
@@ -9,9 +9,9 @@ let mathArr = [];
 
 const mathOperator = () => {
  
-  if(firstOpperand != null && operator != null & secondOpperand != null)
+  if (firstOpperand != null && operator != null && secondOpperand != null)
 
-  switch(operator) {
+  switch (operator) {
     case '+':
      return calcDisplay.value = parseFloat(firstOpperand)  + parseFloat(secondOpperand) ;
       break;
@@ -27,8 +27,8 @@ const mathOperator = () => {
 };
 
 // Number Buttons
-const numbers = document.querySelectorAll('.number')
-for(let i = 0; i < numbers.length; i++){
+const numbers = document.querySelectorAll('.number');
+for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener('click', () => {
 
   document.getElementById('zero').value = 0;
@@ -43,40 +43,37 @@ for(let i = 0; i < numbers.length; i++){
   document.getElementById('nine').value = 9;
   document.getElementById('decimal').value = '.';
 
-if(firstOpperand === mathOperator && operator !== null){
+if (firstOpperand === mathOperator && operator !== null) {
   calcDisplay.value = '';
-  calcDisplay.value  += numbers[i].value;
+  calcDisplay.value += numbers[i].value;
 }
 
- 
- if(operator === null){
+ if (operator === null) {
   calcDisplay.value += numbers[i].value;
   firstOpperand = calcDisplay.value;
-
-
 // TF
- } else if (firstOpperand !== null && operator != null){
-  secondOpperand = numbers[i].value;
-  calcDisplay.value += secondOpperand;
-   
-  
+} else if (firstOpperand !== null && operator != null) {
+ secondOpperand = numbers[i].value;
+  // calcDisplay.value = secondOpperand;
  
-  } else if (firstOpperand !== null && 
-    operator !== null && 
-    secondOpperand !== null){ 
-    firstOpperand = mathOperator();
-     calcDisplay.value = firstOpperand;
-     operator = null;
-     secondOpperand = null;
-  } 
+} else if (
+  firstOpperand !== null &&
+  operator !== null &&
+  secondOpperand !== null
+) {
+  firstOpperand = mathOperator();
+  calcDisplay.value = firstOpperand;
+  operator = null;
+  secondOpperand = null;
+}
 
   console.log(firstOpperand, operator, secondOpperand);
   });
 }
 
 // Operator buttons
- const operators = document.querySelectorAll('.operators')
- for(let x = 0; x < operators.length; x++){
+ const operators = document.querySelectorAll('.operators');
+ for(let x = 0; x < operators.length; x++) {
    operators[x].addEventListener('click', () => {
  
     document.getElementById('divide').value = '/';
@@ -87,7 +84,10 @@ if(firstOpperand === mathOperator && operator !== null){
    calcDisplay.value = '';
 
 
-   if(firstOpperand != null && operator != null & secondOpperand != null){
+   if (
+    firstOpperand != null &&
+    (operator != null) & (secondOpperand != null)
+  ) {
     //  calcDisplay.value = '';
      mathOperator();
       firstOpperand = mathOperator();
@@ -100,17 +100,21 @@ if(firstOpperand === mathOperator && operator !== null){
 
 
 // Equal Button
- const equalBtn = document.querySelector('#equal').addEventListener('click', () => {
-   mathOperator();
-   firstOpperand = mathOperator();
-   secondOpperand = null;
-   operator = null;
- });
+const equalBtn = document
+  .querySelector('#equal')
+  .addEventListener('click', () => {
+    mathOperator();
+    firstOpperand = mathOperator();
+    secondOpperand = null;
+    operator = null;
+  });
 
 // Clear Input
-const clrBtn = document.querySelector('#clearBtn').addEventListener('click', () => {
-  calcDisplay.value = '';
-  firstOpperand = null;
-  secondOpperand = null;
-  operator = null;
-});
+const clrBtn = document
+  .querySelector('#clearBtn')
+  .addEventListener('click', () => {
+    calcDisplay.value = '';
+    firstOpperand = null;
+    secondOpperand = null;
+    operator = null;
+  });
